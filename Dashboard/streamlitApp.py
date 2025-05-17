d8307fc21e14302ade55460788725a97fbd27c4d
JoseSuarezG <josed_5@hotmail.com>
2025-05-17 17:39:29 -0500

    Se actualiza la ubicacion de los archivos

--- Dashboard/streamlitApp.py
+++ Dashboard/streamlitApp.py
@@ -2,13 +2,15 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="ConGas IA", page_icon=":material/edit:",layout="wide")
+st.title("Vista General")
+st.write("Contenido de la vista general.")
 
 # Define las hojas de la aplicación
-vistaGeneral = st.Page("vistaGeneral.py", title="Vista General", icon="🚥")
-perfilCliente = st.Page("perfilCliente.py", title="Perfil Cliente", icon="🧑‍🏫")
-analisisAnomalias = st.Page("analisisAnomalias.py", title="Analisis Anomalias", icon="📊")
-exploracionDatos = st.Page("exploracionDatos.py", title="Exploracion de Datos", icon="📊")
+#vistaGeneral = st.Page("vistaGeneral.py", title="Vista General", icon="🚥")
+#perfilCliente = st.Page("perfilCliente.py", title="Perfil Cliente", icon="🧑‍🏫")
+#analisisAnomalias = st.Page("analisisAnomalias.py", title="Analisis Anomalias", icon="📊")
+#exploracionDatos = st.Page("exploracionDatos.py", title="Exploracion de Datos", icon="📊")
 
 # Define la navegacion de las hojas
-pg = st.navigation([vistaGeneral, perfilCliente, analisisAnomalias])
-pg.run()
+#pg = st.navigation([vistaGeneral, perfilCliente, analisisAnomalias])
+#pg.run()
