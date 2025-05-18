d8817d0606cf5f18ba5eb0138e06089f9ea4bebd
JoseSuarezG <josed_5@hotmail.com>
2025-05-17 19:30:34 -0500

    Mejorando la visualizacion de la imagen

--- Dashboard/Bienvenido.py
+++ Dashboard/Bienvenido.py
@@ -18,7 +18,7 @@ with col2:
 with col1:
     st.image(img_path, use_container_width=True)
     st.write("""ConGas IA \n
-             Aplicación de análisis de datos de consumo de gas \n
+             Aplicación de análisis de datos de consumo de gas
              Versión 1.0""")
     st.write("Desarrollado por: Grupo ConGas")
     st.write("Fecha: 2023-10-01")
